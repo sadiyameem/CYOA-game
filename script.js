ebcb02a1e5ee20728f70af519dea4ab8d0b9e6b1
Sadiya <160082120+sadiyameem@users.noreply.github.com>
2026-01-31 18:29:34 -0500

    set up the js

---
+++ script.js
@@ -0,0 +1,22 @@
+const textElement = document.getElementById('text')
+const optionButtons = document.getElementById('option-buttons')
+
+let state = {}
+
+function startGame() {
+    state = {}
+    showTextNode(1)
+}
+
+function showTextNode(textNodeIndex) {}
+
+function selectOption(option) {}
+
+const textNodes = [
+    {
+        id: 1,
+        text: ''
+    }
+]
+
+startGame()
