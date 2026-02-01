d6405d0be5ec7d8c2456dda8a223cb5a5d4cdc7a
Sadiya <160082120+sadiyameem@users.noreply.github.com>
2026-02-01 12:28:10 -0500

    Added some of the story and comment out some of the errors

--- script.js
+++ script.js
@@ -8,15 +8,103 @@ function startGame() {
     showTextNode(1)
 }
 
-function showTextNode(textNodeIndex) {}
+function showTextNode(textNodeIndex) {
+    const textNode = textNodes.find(textNode => textNode.id === textNodeIndex)
+    textElement.innerText = textNode.text
+    while (optionButtonsElement.firstChild) {
+        optionBUttonselement.removeChild(optionButtonsElement.firstChild)
+    }
 
-function selectOption(option) {}
+    textNode.options.forEach(option => {
+        if (showOption(option)) {
+            const button = document.createElement('button')
+            button.innerText = option.text
+            button.classList.add('btn')
+            button.addEventListener('click', () => selectOption(option))
+            optionButtonsElement.appendChild(button)
+        }
+    })
+}
+
+function showOption(option) {
+    return option.requiredState == null || option.requiredState(state)
+}
+
+function selectOption(option) {
+    const nextTextNodeId = option.nextText
+    if (nextTextNodeId <= 0) {
+        return startGame()
+    }
+    state = Object.assign(state, option.setState)
+    showTextNode(nextTextNodeId)
+}
 
 const textNodes = [
     {
         id: 1,
-        text: ''
-    }
+        text: 'You wake up in a strange place and you see a jar of blue goo near you.',
+        options: [
+            {
+                text: 'Take goo',
+                setState: { blueGoo: true },
+                nextText: 2
+            },
+            {
+                text: 'Leave goo',
+                nextText : 2
+            }
+        ]
+    },
+    // {
+    //     id: 2,
+    //     text: 'You venture forth in search of answers to where you are when you come across a merchant.'
+    //     // options: [
+    //     //     {
+    //     //         text: 'Trade the goo for a sword',
+    //     //         requiredState: (currentState) => currentState.blueGoo,
+    //     //         setState: { blueGoo: false, sword: true},
+    //     //         nextText: 3
+    //     //     },
+    //     //     {
+    //     //         text: 'Trade the goo for a shield',
+    //     //         requiredState: (currentState) => currentState.blueGoo,
+    //     //         setState: { blueGoo: false, shield: true},
+    //     //         nextText: 3
+    //     //     }
+    //         // {
+    //         //    text: 'Ignore the merchant',
+    //         //    nextText: 3 
+    //         // }
+    //     ]
+    // },
+    {
+        id: 3,
+        text: 'After leaving the merchant you start to feel tired and stumble upon a small town next to a dangerous looking castle.',
+        options: [
+            {
+                text: 'Explore the castle',
+                nextText: 4,
+            },
+            {
+                text: 'Find a room to sleep at in the town',
+                nextText: 5
+            },
+            {
+                text: 'Find some hay in a stable to sleep in',
+                nextText: 6
+            }
+        ]
+    },
+    // {
+    //     id: 4,
+    //     text: 'You are so tried that you fall asleep while exploring the castle and are killed by some terrible monster in your sleep.'
+    //     options: [
+    //         {
+    //             text: 'Restart',
+    //             nextText: -1
+    //         }
+    //     ]
+    // }
 ]
 
 startGame()
